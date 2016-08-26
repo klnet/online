01be256668a155188ca06d4c3d09140679fc79b5
Dao Hoang Son <daohoangson@gmail.com>
2016-08-26 16:04:50 +0700

    Added .search-form via js

--- clientscript/klnet.online.js
+++ clientscript/klnet.online.js
@@ -6,3 +6,23 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-9039292-1', 'auto');
 ga('send', 'pageview');
 // End Google Analytics
+
+window.onload = function() {
+	var head = document.getElementsByTagName('head')[0];
+	var css = document.createElement('style');
+	css.innerText = '.bannerlogo { position: relative; }'
+		+ '.search-form { bottom: 0; position: absolute; width: 100%; }'
+		+ '.search-form > form { float: right; display: block; margin: 10px 25px; width: 25%; }'
+		+ '.search-form input[name=q] { font-size: 3em; width: 100%; }'
+		+ '@media (max-width: 600px) { .search-form > form { width: 75%; } }';
+	head.appendChild(css);
+
+	var isSearchHtml = window.location.pathname.substr(-11) === 'search.html';
+	if (!isSearchHtml) {
+		var bannerLogo = document.getElementsByClassName('bannerlogo')[0];
+		bannerLogo.innerHTML = '<div class="search-form">'
+			+ '<form action="search.html" method="get">'
+			+ '<input type="search" name="q" placeholder="Search" />'
+			+ '</form></div>';
+	}
+};
